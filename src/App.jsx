3a700b43336724b717f1c24d1e46d71b99e3ddaa
qHollow <nighthunter2015@gmail.com>
2021-07-09 16:15:04 +0300

    feat: add eslint config, add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import GlobalStyle from "./globalStyle";
-import { Wrapper, WrapperColumn } from "./style";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GlobalStyle from './globalStyle';
+import { Wrapper, WrapperColumn } from './style';
 
-import Header from "./components/Header";
-import Aside from "./components/Aside";
+import Header from './components/Header';
+import Aside from './components/Aside';
 
-import Dashboard from "./pages/Dashboard";
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <Router>
-        <Wrapper>
-          <Header />
-          <WrapperColumn>
-            <Aside />
-            <Switch>
-              <Route path="/" exact>
-                <Dashboard />
-              </Route>
-              <Route>
-                <Dashboard />
-              </Route>
-            </Switch>
-          </WrapperColumn>
-        </Wrapper>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<Router>
+				<Wrapper>
+					<Header />
+					<WrapperColumn>
+						<Aside />
+						<Switch>
+							<Route path='/' exact>
+								<Dashboard />
+							</Route>
+							<Route>
+								<Dashboard />
+							</Route>
+						</Switch>
+					</WrapperColumn>
+				</Wrapper>
+			</Router>
+		</>
+	);
 };
 
 export default App;
